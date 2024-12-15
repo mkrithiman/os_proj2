7fbb33fd002926769de98476087d155448a6a72f
hsriram3 <harivfh@gmaile.com>
2024-12-15 01:26:04 +0000

    From hari with ipc

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,10 +2,30 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+#include "userprog/syscall.h"
 
+/* the file that this process has */
+struct process_file {
+	int fd;
+	struct file *file;
+	struct list_elem elem;
+};
+
+/* original function from pintos */
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+
+/* function header added for project 2: process_file struct */
+int process_add_file (struct file *f, struct thread * t);
+struct file* process_get_file (int fd, struct thread * t);
+void process_close_file (int fd, struct thread * t);
+
+/* function header added for child_process struct */
+void add_child_process (int pid, struct thread * t);
+struct child_process* get_child_process (int pid, struct thread * t);
+void remove_child_process (struct child_process *cp);
+void remove_all_children (struct thread * t);
 
 #endif /* userprog/process.h */
